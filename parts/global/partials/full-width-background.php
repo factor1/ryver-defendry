19dbe17752e7308c31ad00e2659261dda1a6d303
Melanie <mlpatt23@gmail.com>
2018-11-29 14:28:35 -0700

    Started working on footer rework

--- parts/global/partials/full-width-background.php
+++ parts/global/partials/full-width-background.php
@@ -10,7 +10,7 @@ $btn = get_sub_field('button'); ?>
 
 <section class="container full-width-background" style="background: url('<?php echo $img[0]; ?>') left top/cover no-repeat">
   <div class="row">
-    <div class="col-12 text-center">
+    <div class="col-12 text-center" data-aos="fade-up" data-aos-duration="800">
       <h4><?php echo $headline; ?></h4>
 
       <a href="<?php echo esc_url($btn['url']); ?>" class="button button--white" role="link" title="<?php echo $btn['title']; ?>">
