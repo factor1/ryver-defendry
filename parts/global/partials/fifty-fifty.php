b394cfe83e8e84515294bf98716794e86eaa53c0
Melanie <mlpatt23@gmail.com>
2018-11-29 12:40:52 -0700

    Reworked 50/50 section and styles

--- parts/global/partials/fifty-fifty.php
+++ parts/global/partials/fifty-fifty.php
@@ -1,118 +1,54 @@
 <?php
-
 //vars
+$bgToggle = get_sub_field('background_toggle');
 $header = get_sub_field('header');
 $position = get_sub_field('image_position');
 $img_id = get_sub_field('image');
 $img = wp_get_attachment_image_src($img_id, 'fifty-fifty');
 $content_header = get_sub_field('content_header');
-$text_row = get_sub_field('text_row');
+$content = get_sub_field('content');
+$btnToggle = get_sub_field('button_toggle');
+$btn = get_sub_field('button');
 
-?>
-<section class="container fifty-fifty">
-    <div class="row">
-      <div class="sm-col-11 col-10 col-centered">
-        <div class="row">
+// Conditional classes/styles
+$rowClass = $position == 'left' ? ' row--reverse' : '';
+$bgClass = $bgToggle ? ' background' : '';
+$imgAnimation = $position == 'left' ? 'fade-right' : 'fade-left';
+$textAnimation = $position == 'right' ? 'fade-right' : 'fade-left';  ?>
 
-          <?php if($header) : ?>
-            <div class="col-12">
-              <h1 class="text-center">
-                <?php echo $header; ?>
-              </h1>
-            </div>
-          <?php endif; ?>
+<section class="container fifty-fifty<?php echo $bgClass; ?>">
+  <div class="row">
+    <div class="sm-col-11 col-10 col-centered">
+      <div class="row row--align-items-center<?php echo $rowClass; ?>">
 
-          <?php if($position === "left") : ?>
+        <?php if($header) : ?>
+          <div class="col-12" data-aos="fade-down" data-aos-duration="800" data-aos-delay="500">
+            <h1 class="text-center">
+              <?php echo $header; ?>
+            </h1>
+          </div>
+        <?php endif; ?>
 
-            <div class="sm-col-12 col-6 columns stretch fifty-fifty__image-wrap">
-              <div class="fifty-fifty__image" data-aos="fade-right" data-aos-duration="500">
-                <img src="<?php echo $img[0]; ?>" />
-              </div>
-            </div>
+        <div class="col-6 sm-text-center" data-aos="<?php echo $textAnimation; ?>">
+          <?php if($content_header) : ?>
+            <h2><?php echo $content_header; ?></h2>
+          <?php endif;
 
-            <div class="sm-col-12 col-6 columns stretch fifty-fifty__content-wrap">
+          echo $content; ?>
+        </div>
 
-              <div class="row fifty-fifty__content" data-aos="fade-left" data-aos-delay="700">
-
-                <?php if($content_header) : ?>
-                  <h2>
-                    <?php echo $content_header; ?>
-                  </h2>
-                <?php endif; ?>
-
-                <?php foreach($text_row as $i => $item) {
-
-                  // variables
-                  $icon = $text_row[$i]['text_row_icon'];
-                  //$icon = wp_get_attachment_image_src($icon_id, 'full');
-                  $text = $text_row[$i]['text_row_text'];
-
-                ?>
-
-                <div class="columns__text-wrap">
-                  <?php if($icon) : ?>
-                    <div class="columns__icon">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/<?php echo $icon; ?>.svg" />
-                    </div>
-                  <?php endif; ?>
-                  <div class="columns__text">
-                    <?php echo $text; ?>
-                  </div>
-                </div>
-
-                <?php } ?>
-
-              </div>
-
-            </div>
-
-          <?php elseif($position === "right") : ?>
-
-            <div class="sm-col-12 col-6 columns stretch fifty-fifty__content-wrap">
-
-              <div class="row fifty-fifty__content" data-aos="fade-right" data-aos-delay="700">
-
-                <?php if($content_header) : ?>
-                  <h2>
-                    <?php echo $content_header; ?>
-                  </h2>
-                <?php endif; ?>
-
-                <?php foreach($text_row as $i => $item) {
-
-                  // variables
-                  $icon = $text_row[$i]['text_row_icon'];
-                  //$icon = wp_get_attachment_image_src($icon_id, 'full');
-                  $text = $text_row[$i]['text_row_text'];
-
-                ?>
-
-                <div class="columns__text-wrap">
-                  <?php if($icon) : ?>
-                    <div class="columns__icon">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/<?php echo $icon; ?>.svg" />
-                    </div>
-                  <?php endif; ?>
-                  <div class="columns__text">
-                    <?php echo $text; ?>
-                  </div>
-                </div>
-
-                <?php } ?>
-
-              </div>
-
-            </div>
-
-            <div class="sm-col-12 col-6 columns stretch fifty-fifty__image-wrap">
-              <div class="fifty-fifty__image" data-aos="fade-left" data-aos-duration="500">
-                <img src="<?php echo $img[0]; ?>" />
-              </div>
-            </div>
-
-          <?php endif; ?>
-
+        <div class="col-5 sm-col-8 sm-col-centered" data-aos="<?php echo $imgAnimation; ?>" data-aos-duration="500">
+          <img src="<?php echo $img[0]; ?>" />
         </div>
       </div>
+
+      <?php if( $btnToggle ) : ?>
+        <div class="text-center" data-aos="fade-up" data-aos-duration="500">
+          <a href="<?php echo esc_url($btn['url']); ?>" class="button button--primary" role="link" title="<?php echo $btn['title']; ?>">
+            <?php echo $btn['title']; ?>
+          </a>
+        </div>
+      <?php endif; ?>
     </div>
+  </div>
 </section>
