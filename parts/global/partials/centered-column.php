05318d74d57e8d47d3ab5f2f31394d944b122eee
Melanie <mlpatt23@gmail.com>
2018-11-29 14:02:09 -0700

    Adjusted styles for centered column section

--- parts/global/partials/centered-column.php
+++ parts/global/partials/centered-column.php
@@ -4,11 +4,17 @@
  */
 
 // Custom Fields
+$bgToggle = get_sub_field('background_toggle');
 $headline = get_sub_field('headline');
+$btnToggle = get_sub_field('button_toggle');
+$btn = get_sub_field('button');
+
+// Conditional classes/styles
+$bgClass = $bgToggle ? ' background' : '';
 
 if( have_rows('content_blocks') ) : ?>
 
-  <section class="container centered-column">
+  <section class="container centered-column<?php echo $bgClass; ?>">
     <div class="row">
       <div class="col-12 text-center" data-aos="fade-up" data-aos-duration="800">
         <h1><?php echo $headline; ?></h1>
@@ -18,15 +24,26 @@ if( have_rows('content_blocks') ) : ?>
 
         <?php while( have_rows('content_blocks') ) : the_row();
           $img = wp_get_attachment_image_src(get_sub_field('image'), 'column');
-          $content = get_sub_field('content'); ?>
+          $content = get_sub_field('content');
+          $divider = get_sub_field('divider_toggle');
 
-          <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
+          $dotClass = $divider ? ' class="dot"' : ''; ?>
+
+          <div<?php echo $dotClass; ?> data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
             <img src="<?php echo $img[0]; ?>" alt="Column image">
 
             <?php echo $content; ?>
           </div>
 
         <?php endwhile; ?>
+
+        <?php if( $btnToggle ) : ?>
+          <div class="text-center" data-aos="fade-up" data-aos-duration="500">
+            <a href="<?php echo esc_url($btn['url']); ?>" class="button button--primary" role="link" title="<?php echo $btn['title']; ?>">
+              <?php echo $btn['title']; ?>
+            </a>
+          </div>
+        <?php endif; ?>
 
       </div>
     </div>
