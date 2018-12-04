2fbfd0ce5a7fb5e2d33a67d71c01409e44df8884
Melanie <mlpatt23@gmail.com>
2018-12-04 11:36:21 -0700

    Adjusted column settings in home hero

--- parts/home/hero.php
+++ parts/home/hero.php
@@ -13,7 +13,7 @@
       <img class="ryver-power" src="<?php echo get_template_directory_uri(); ?>/assets/img/PoweredByRyver-Logo-DarkBG-2.svg" />
     </a>
     <div class="row">
-      <div class="col-8 md-col-10 sm-text-center">
+      <div class="col-7 md-col-8 sm-text-center">
         <div class="hero__logo sm-text-center" data-aos="fade-down" data-aos-delay="800" data-aos-duration="800">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/defendry-logo.svg" alt="Defendry" />
         </div>
