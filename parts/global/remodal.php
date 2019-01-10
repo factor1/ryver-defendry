7d33bb0a0ff575adda5bfe20fcbac7e6e7b452de
Dana Malkin <dana@danamalkin.com>
2019-01-10 14:20:26 -0700

    added new acf driven modals to support gravity forms and styled them

--- parts/global/remodal.php
+++ parts/global/remodal.php
@@ -1,5 +1,13 @@
 <!-- Tell Me More -->
 
+<?php
+  // variables
+  $tmm_title = get_field('tmm_title', 'option');
+  $tmm_description = get_field('tmm_description', 'option');
+  $tmm_content = get_field('tmm_content', 'option');
+
+?>
+
 <div class="remodal" data-remodal-id="tell-me-more">
   <div class="remodal__wrap">
     <div class="remodal__header">
@@ -7,82 +15,32 @@
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/baseline-add_circle_outline.svg" />
         <div class="close-bg"></div>
       </div>
-      <h2>Tell Me About Defendry</h2>
+      <?php if($tmm_title) : ?>
+        <h2>
+          <?php echo $tmm_title; ?>
+        </h2>
+      <?php endif; ?>
     </div>
     <div class="remodal__body">
-      <!-- Begin MailChimp Signup Form -->
-      <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-      <style type="text/css">
-          #mc_embed_signup {
-              background: #fff;
-              clear: left;
-              font: 14px Helvetica, Arial, sans-serif;
-          }
-          /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-      	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-      </style>
-      <div id="mc_embed_signup">
-          <form action="https://ryver.us5.list-manage.com/subscribe/post?u=bbfac7d1353c0b75fa67a2cab&amp;id=aeda8d9674" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-              <div id="mc_embed_signup_scroll">
-
-                  <div class="mc-field-group">
-                      <input type="email" value="" name="EMAIL" placeholder="Email Address" class="required email" id="mce-EMAIL">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="FNAME" placeholder="First Name" class="required" id="mce-FNAME">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="LNAME" placeholder="Last Name" class="required" id="mce-LNAME">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" name="PHONE" placeholder="Phone Number (optional)" class="" value="" id="mce-PHONE">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="TEAM" placeholder="Company or Team Name (optional)" class="" id="mce-TEAM">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="TITLE" placeholder="Position/Title (optional) " class="" id="mce-TITLE">
-                  </div>
-                  <div id="mce-responses" class="clear">
-                      <div class="response" id="mce-error-response" style="display:none"></div>
-                      <div class="response" id="mce-success-response" style="display:none"></div>
-                  </div>
-                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                      <input type="text" name="b_bbfac7d1353c0b75fa67a2cab_aeda8d9674" tabindex="-1" value="">
-                  </div>
-                  <div class="clear">
-                      <input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button button--primary">
-                  </div>
-              </div>
-          </form>
-      </div>
-      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-      <script type='text/javascript'>
-          (function($) {
-              window.fnames = new Array();
-              window.ftypes = new Array();
-              fnames[0] = 'EMAIL';
-              ftypes[0] = 'email';
-              fnames[1] = 'FNAME';
-              ftypes[1] = 'text';
-              fnames[2] = 'LNAME';
-              ftypes[2] = 'text';
-              fnames[4] = 'PHONE';
-              ftypes[4] = 'phone';
-              fnames[5] = 'TEAM';
-              ftypes[5] = 'text';
-              fnames[3] = 'TITLE';
-              ftypes[3] = 'text';
-          }(jQuery));
-          var $mcj = jQuery.noConflict(true);
-      </script>
-      <!--End mc_embed_signup-->
+      <?php if($tmm_description) : ?>
+        <p>
+          <?php echo $tmm_description; ?>
+        </p>
+      <?php endif; ?>
+      <?php echo $tmm_content; ?>
     </div>
   </div>
 </div>
 
 <!-- Learn More -->
+
+<?php
+  // variables
+  $lm_title = get_field('lm_title', 'option');
+  $lm_description = get_field('lm_description', 'option');
+  $lm_content = get_field('lm_content', 'option');
+
+?>
 
 <div class="remodal" data-remodal-id="learn-more">
   <div class="remodal__wrap">
@@ -91,90 +49,19 @@
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/baseline-add_circle_outline.svg" />
         <div class="close-bg"></div>
       </div>
-      <h2>Become an Implementation Partner</h2>
+      <?php if($lm_title) : ?>
+        <h2>
+          <?php echo $lm_title; ?>
+        </h2>
+      <?php endif; ?>
     </div>
     <div class="remodal__body">
-      <p>Please tell us a bit about yourself, and we'll get back to you as soon as possible!</p>
-      <!-- Begin MailChimp Signup Form -->
-      <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-      <style type="text/css">
-          #mc_embed_signup {
-              background: #fff;
-              clear: left;
-              font: 14px Helvetica, Arial, sans-serif;
-          }
-          /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-      	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-      </style>
-      <div id="mc_embed_signup">
-          <form action="https://ryver.us5.list-manage.com/subscribe/post?u=bbfac7d1353c0b75fa67a2cab&amp;id=c265442b6b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-              <div id="mc_embed_signup_scroll">
-                  <div class="mc-field-group">
-                      <input type="email" value="" name="EMAIL" placeholder="Email Address" class="required email" id="mce-EMAIL">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="FNAME" placeholder="First Name" class="required" id="mce-FNAME">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="LNAME" placeholder="Last Name" class="required" id="mce-LNAME">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" name="PHONE" placeholder="Phone Number (optional)" class="" value="" id="mce-PHONE">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="TEAM" placeholder="Company or Team Name (optional)" class="" id="mce-TEAM">
-                  </div>
-                  <div class="mc-field-group">
-                      <input type="text" value="" name="TITLE" placeholder="Position/Title (optional)" class="" id="mce-TITLE">
-                  </div>
-                  <div class="mc-field-group input-group">
-                      <strong>Systems You Currently Install  <span class="asterisk">*</span></strong>
-                      <ul>
-                        <li><input style="margin-right:10px" type="checkbox" value="1" name="group[14553][1]" id="mce-group[14553]-14553-0"><label for="mce-group[14553]-14553-0">IoT Systems</label></li>
-                        <li><input style="margin-right:10px" type="checkbox" value="2" name="group[14553][2]" id="mce-group[14553]-14553-1"><label for="mce-group[14553]-14553-1">Security Systems</label></li>
-                        <li><input style="margin-right:10px" type="checkbox" value="4" name="group[14553][4]" id="mce-group[14553]-14553-2"><label for="mce-group[14553]-14553-2">Emergency Response Systems</label></li>
-                        <li><input style="margin-right:10px" type="checkbox" value="8" name="group[14553][8]" id="mce-group[14553]-14553-3"><label for="mce-group[14553]-14553-3">Accounting Systems</label></li>
-                        <li><input style="margin-right:10px" type="checkbox" value="16" name="group[14553][16]" id="mce-group[14553]-14553-4"><label for="mce-group[14553]-14553-4">Phone Systems</label></li>
-                        <li><input style="margin-right:10px" type="checkbox" value="32" name="group[14553][32]" id="mce-group[14553]-14553-5"><label for="mce-group[14553]-14553-5">Other</label></li>
-                      </ul>
-                  </div>
-                  <div id="mce-responses" class="clear">
-                      <div class="response" id="mce-error-response" style="display:none"></div>
-                      <div class="response" id="mce-success-response" style="display:none"></div>
-                  </div>
-                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                      <input type="text" name="b_bbfac7d1353c0b75fa67a2cab_c265442b6b" tabindex="-1" value="">
-                  </div>
-                  <div class="clear">
-                      <input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button button--secondary">
-                  </div>
-              </div>
-          </form>
-      </div>
-      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-      <script type='text/javascript'>
-          (function($) {
-              window.fnames = new Array();
-              window.ftypes = new Array();
-              fnames[0] = 'EMAIL';
-              ftypes[0] = 'email';
-              fnames[1] = 'FNAME';
-              ftypes[1] = 'text';
-              fnames[2] = 'LNAME';
-              ftypes[2] = 'text';
-              fnames[4] = 'PHONE';
-              ftypes[4] = 'phone';
-              fnames[3] = 'TEAM';
-              ftypes[3] = 'text';
-              fnames[5] = 'TITLE';
-              ftypes[5] = 'text';
-              fnames[6] = 'MMERGE6';
-              ftypes[6] = 'radio';
-          }(jQuery));
-          var $mcj = jQuery.noConflict(true);
-      </script>
-      <!--End mc_embed_signup-->
+      <?php if($lm_description) : ?>
+        <p>
+          <?php echo $lm_description; ?>
+        </p>
+      <?php endif; ?>
+      <?php echo $lm_content; ?>
     </div>
   </div>
 </div>
