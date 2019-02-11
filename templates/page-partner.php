19949c3614f4330e3355cac045707912e97177b5
Matt Adams <mattadams@factor1studios.com>
2019-02-11 11:10:43 -0700

    Aggressive manual merger

---
+++ templates/page-partner.php
@@ -0,0 +1,11 @@
+<?php
+  /**
+   * Template Name: Partner
+   *
+   */
+  get_header();
+
+  get_template_part('parts/partner/hero');
+  get_template_part('parts/global/flexible-content');
+
+  get_footer();
