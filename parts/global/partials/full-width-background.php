95f3a88c85b8a249925ab5e31df2f5dd08fb780f
Matt Adams <mattadams@factor1studios.com>
2019-02-11 11:29:51 -0700

    Revert "Merge branch 'master' into page-partners"
    This reverts commit 38de82a4faa77792bff94d219a17c250a8cae121.

--- parts/global/partials/full-width-background.php
+++ parts/global/partials/full-width-background.php
@@ -6,12 +6,18 @@
 // Custom Fields
 $img = wp_get_attachment_image_src(get_sub_field('background'), 'video_bg');
 $headline = get_sub_field('headline');
-$btn = get_sub_field('button'); ?>
+$copy = get_sub_field('support_content');
+$btn = get_sub_field('button');
+ ?>
+
+
 
 <section class="container full-width-background" style="background: url('<?php echo $img[0]; ?>') left top/cover no-repeat">
   <div class="row">
     <div class="col-12 text-center" data-aos="fade-up" data-aos-duration="800">
       <h4><?php echo $headline; ?></h4>
+      
+      <?php if($copy); echo $copy;?>
 
       <a href="<?php echo esc_url($btn['url']); ?>" class="button button--white" role="link" title="<?php echo $btn['title']; ?>">
         <?php echo $btn['title']; ?>
