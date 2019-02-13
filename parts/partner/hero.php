1d6a99e34573df69e9a4cdd728cecd015b1f55eb
Melanie <mlpatt23@gmail.com>
2019-02-13 14:36:52 -0700

    Added hero content class to partner hero to address padding difference

--- parts/partner/hero.php
+++ parts/partner/hero.php
@@ -10,7 +10,7 @@
 
   <section class="container hero" data-aos="fade-in" style="background:url('<?php echo $bg_img[0]; ?>') center center/cover no-repeat;">
     <div class="row hero__row">
-      <div class="col-7 md-col-8 sm-text-center">
+      <div class="col-7 md-col-8 sm-text-center hero__row__content">
         <div class="hero__logo sm-text-center" data-aos="fade-down" data-aos-delay="800" data-aos-duration="800">
 	        <a href="<?php bloginfo('url');?>">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/defendry-tagline-dark.svg" alt="Defendry" />
@@ -19,7 +19,7 @@
         <h1 class="hero__headline" data-aos="fade-down" data-aos-delay="1000" data-aos-duration="600">
           <?php echo $header; ?>
         </h1>
-        
+
       </div>
     </div>
   </section>
