eeb797d8b7be22eb70668d83aae999bd6076456f
Dana Malkin <dana@danamalkin.com>
2019-02-28 22:00:32 -0700

    Bumps theme version to 1.2.62

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-  define( 'THEME_VERSION', '1.2.618' );
+  define( 'THEME_VERSION', '1.2.628' );
 
   /**
    * Load tweaks
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------------*/
 // Theme information (name, starting theme version)
 var theme        = 'defendry', // will be autocompleted by prelude-init
-    version      = '1.2.61'; // updated with gulp version task
+    version      = '1.2.62'; // updated with gulp version task
 
 // Set the paths you will be working with
 var phpFiles     = ['./**/*.php', './*.php'],
