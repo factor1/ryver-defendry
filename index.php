f1b2e828049c643828e1dcd6db7d5f6b18a5e28d
Dana Malkin <dana@danamalkin.com>
2019-03-27 13:27:23 -0700

    added index page and adjusted anchor styles in full width block

--- index.php
+++ index.php
@@ -4,43 +4,78 @@
    */
   get_header();
 
-  if ( have_posts() ) : ?>
+  // get_template_part('parts/global/flexible-content'); ?>
 
-    <section class="container animsition" data-animsition-in-class="fade-in-right-lg" data-animsition-in-duration="400" data-animsition-out-class="fade-out-left-lg" data-animsition-out-duration="400">
-      <div class="row row--justify-content-center row--align-content-center work-page">
-        <div class="col-4 sm-col-12 work-titles-container">
-          <ol data-aos="zoom-in-left" data-aos-duration="1200">
-          <?php $num = 400; ?>
-          <?php while ( have_posts() ) : the_post(); ?>
-            <li class="post-title" id="<?php echo get_the_ID(); ?>" data-aos="zoom-in-left" data-aos-delay="500" data-aos-duration="<?php echo $num; ?>">
-              <?php the_title(); ?>
-            </li>
-            <?php $num = $num + 200; ?>
-          <?php endwhile; ?>
-          </ol>
+
+
+  <?php if ( have_posts() ) : ?>
+
+    <section class="container blog">
+      <div class="row">
+        <div class="sm-col-11 col-10 col-centered">
+          <div class="blog__header text-center">
+            <h1>Latest News</h1>
+          </div>
         </div>
-        <div class="col-6 offset-1 work-image-container" data-aos="fade-right" data-aos-delay="400">
-          <?php while ( have_posts() ) : the_post();
-            // Variables
-            $id = get_the_ID();
-            $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'feature' );
-            $url = $thumb['0'];
-          ?>
-            <a class="image-wrap animsition-link" id="<?php echo $id; ?>" href="<?php the_permalink(); ?>" data-animsition-out-class="fade-out-up" data-animsition-out-duration="400">
-              <div class="featured-image" style="background:url(<?php echo $url; ?>) center center/cover no-repeat" data-aos="zoom-in-right" data-aos-delay="500">
-              <h3 class="text-hover text-center">View Project</h3>
-              </div>
-              <img class="red-icon" id="icon4" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.svg" />
-              <div class="white-lines"></div>
-            </a>
-          <?php endwhile; ?>
-        </div>
-
-        <img class="red-icon" id="icon1" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" data-aos="fade-left" data-aos-delay="100" data-aos-duration="800" />
-        <img class="red-icon" id="icon2" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" data-aos="fade-left" data-aos-delay="100" data-aos-duration="1600"/>
-        <img class="red-icon" id="icon3" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" data-aos="fade-left" data-aos-delay="100" data-aos-duration="2400"/>
-
       </div>
+      <div class="row">
+        <div class="sm-col-11 col-12 col-centered">
+
+          <?php while ( have_posts() ) : the_post(); ?>
+
+          <div class="row blog__single-row">
+
+            <?php
+              // variables
+              $thumbnail = get_the_post_thumbnail();
+
+              if ( empty($thumbnail) ) :
+            ?>
+              <div class="sm-col-11 col-4 col-centered columns">
+                <a class="no-underline" href="<?php echo get_permalink(); ?>">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/defendry-tagline-dark.svg" alt="Defendry Logo" />
+                </a>
+              </div>
+            <?php else : ?>
+              <div class="sm-col-11 col-4 col-centered columns">
+                <a class="no-underline" href="<?php echo get_permalink(); ?>">
+                  <img src="<?php the_post_thumbnail_url( 'testimonial' ); ?>" />
+                </a>
+              </div>
+            <?php endif; ?>
+
+            <div class="sm-col-11 col-8 col-centered columns" data-aos="fade-left" data-aos-duration="800">
+              <a class="no-underline" href="<?php echo get_permalink(); ?>">
+                <h5>
+                  <?php the_title(); ?>
+                </h5>
+              </a>
+              <br />
+              <ul class="list-items categories">
+                <?php
+                  $categories = get_the_category();
+                  foreach ( $categories as $category ) {
+                    echo '<li><a href="' . get_category_link( $category->term_id ) . '">' . $category->name . '</a></li>';
+                  }
+                ?>
+              </ul>
+              <p>
+                <?php echo get_the_excerpt(); ?>
+              </p>
+              <a class="button button--primary" href="<?php echo get_permalink(); ?>">Read More</a>
+            </div>
+
+          </div>
+
+          <?php endwhile; ?>
+        </div>
+      </div>
+
+      <?php numeric_pagination();
+
+      else :
+        echo '<h2>Sorry, no posts have been found</h2>'; ?>
+
     </section>
 
   <?php endif;
