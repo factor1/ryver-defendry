2796b868563f4ca151740a8c265aa7b709ee1ec0
Dana Malkin <dana@danamalkin.com>
2019-04-29 07:54:00 -0700

    Bumps theme version to 1.4.28

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------------*/
 // Theme information (name, starting theme version)
 var theme        = 'defendry', // will be autocompleted by prelude-init
-    version      = '1.4.27'; // updated with gulp version task
+    version      = '1.4.28'; // updated with gulp version task
 
 // Set the paths you will be working with
 var phpFiles     = ['./**/*.php', './*.php'],
