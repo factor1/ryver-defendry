dca153068f83a09f9d3103d39302b152eb64fd34
Dana Malkin <dana@danamalkin.com>
2019-05-03 08:00:58 -0700

    allow relevanssi plugin to work with yoast seo

--- inc/tweaks.php
+++ inc/tweaks.php
@@ -150,3 +150,10 @@
   	</style>';
 	}
 	add_action('login_head', 'custom_loginlogo');
+
+  // Allow Relevanssi to work with Yoast SEO
+  add_filter( 'relevanssi_modify_wp_query', 'rlv_meta_fix', 99 );
+  function rlv_meta_fix( $q ) {
+  	$q->set( 'meta_query', array() );
+  	return $q;
+  }
