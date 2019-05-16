cda6f28a9698ff5fc82a136a914db898d1406b07
Dana Malkin <dana@danamalkin.com>
2019-05-16 14:51:37 -0700

    added in new acf options for two column block and centered column block

--- parts/global/partials/two-column-layout.php
+++ parts/global/partials/two-column-layout.php
@@ -4,12 +4,18 @@
  */
 
 // Custom Fields
+$bgToggle = get_sub_field('background_toggle');
+$bgFlip = get_sub_field('flip_background_image');
 $headline = get_sub_field('headline');
+
+// Conditional classes/styles
+$bgClass = $bgToggle ? ' background' : '';
+$bgFlipClass = $bgFlip ? ' background-flip' : '';
 
 if( have_rows('columns') ) :
   $i = 1; ?>
 
-  <section class="container two-column-layout">
+  <section class="container two-column-layout<?php echo $bgClass; echo $bgFlipClass; ?>">
     <div class="row row--justify-content-space-around">
       <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="300" data-aos-duration="800">
         <h1><?php echo $headline; ?></h1>
@@ -17,12 +23,14 @@ if( have_rows('columns') ) :
 
       <?php while( have_rows('columns') ) : the_row();
         $img = wp_get_attachment_image_src(get_sub_field('image'), '');
+        $imgSizeChoice = get_sub_field('image_size');
+        $imgSize = $imgSizeChoice == 'small' ? 'img--small' : 'img';
         $headline = get_sub_field('column_headline');
         $content = get_sub_field('column_content');
         $animation = $i == 1 ? 'fade-right' : 'fade-left'; ?>
 
         <div class="col-5 sm-col-10 sm-col-centered text-center" data-aos="<?php echo $animation; ?>" data-aos-delay="300" data-aos-duration="800">
-          <div class="two-column-layout__img" style="background: url('<?php echo $img[0]; ?>') center/contain no-repeat"></div>
+          <div class="two-column-layout__<?php echo $imgSize; ?>" style="background: url('<?php echo $img[0]; ?>') center/contain no-repeat"></div>
 
           <h2><?php echo $headline; ?></h2>
 
