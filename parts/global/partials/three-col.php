637bfa74d363b9ee1cf4551c9baddffdb847cc78
Dana Malkin <dana@danamalkin.com>
2019-05-16 16:26:54 -0700

    restyled 3 col block

--- parts/global/partials/three-col.php
+++ parts/global/partials/three-col.php
@@ -5,6 +5,7 @@ $columns = get_sub_field('columns');
 $header = get_sub_field('header');
 
 ?>
+
 <section class="container three-col">
   <div class="row">
     <div class="sm-col-11 col-12 columns col-centered">
@@ -16,31 +17,31 @@ $header = get_sub_field('header');
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-10 sm-col-11 col-centered">
-      <div class="sm-block-grid-1 block-grid-3">
-        <?php foreach($columns as $i => $item) {
-          // variables
-          $load = 700;
+  <?php if( have_rows('columns') ) : ?>
 
-          $img = $columns[$i]['image'];
-          //$img = wp_get_attachment_image_src($img_id, 'fifty-fifty');
-          $text = $columns[$i]['text'];
+    <div class="row">
+      <div class="col-10 sm-col-11 col-centered">
+        <div class="sm-block-grid-1 md-block-grid-2 block-grid-3 three-col__grid">
+          <?php while( have_rows('columns') ) : the_row();
+            // variables
+            $load = 100;
+            $img = wp_get_attachment_image_src(get_sub_field('image'), '');
+            $text = get_sub_field('text');
 
-        ?>
-          <div class="col stretch text-center" data-aos="fade-in" data-aos-duration="400" data-aos-delay="<?php echo $load; ?>">
-            <div class="col-wrap">
-              <?php if($img) : ?>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/<?php echo $img; ?>.svg" />
-              <?php endif; ?>
-              <?php echo $text; ?>
+          ?>
+            <div class="col stretch text-center" data-aos="fade-in" data-aos-duration="400" data-aos-delay="<?php echo $load; ?>">
+              <div class="col-wrap">
+                <div class="three-col-layout__img" style="background: url('<?php echo $img[0]; ?>') center/contain no-repeat"></div>
+                <?php echo $text; ?>
+              </div>
             </div>
-          </div>
-        <?php $load += 200; ?>
-        <?php } ?>
+          <?php $load += 50; ?>
+          <?php endwhile; ?>
 
+        </div>
       </div>
     </div>
-  </div>
+
+  <?php endif; ?>
 
 </section>
