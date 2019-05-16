17cb2c5c860a797f479ad03ccf56240cc3a4b61f
Dana Malkin <dana@danamalkin.com>
2019-05-16 16:36:18 -0700

    small style fix to 3 col images

--- parts/global/partials/three-col.php
+++ parts/global/partials/three-col.php
@@ -22,20 +22,20 @@ $header = get_sub_field('header');
     <div class="row">
       <div class="col-10 sm-col-11 col-centered">
         <div class="sm-block-grid-1 md-block-grid-2 block-grid-3 three-col__grid">
+          <?php $load = 100; ?>
           <?php while( have_rows('columns') ) : the_row();
             // variables
-            $load = 100;
             $img = wp_get_attachment_image_src(get_sub_field('image'), '');
             $text = get_sub_field('text');
 
           ?>
-            <div class="col stretch text-center" data-aos="fade-in" data-aos-duration="400" data-aos-delay="<?php echo $load; ?>">
+            <div class="col stretch text-center" data-aos="fade-up" data-aos-duration="400" data-aos-delay="<?php echo $load; ?>">
               <div class="col-wrap">
                 <div class="three-col-layout__img" style="background: url('<?php echo $img[0]; ?>') center/contain no-repeat"></div>
                 <?php echo $text; ?>
               </div>
             </div>
-          <?php $load += 50; ?>
+          <?php $load += 100; ?>
           <?php endwhile; ?>
 
         </div>
