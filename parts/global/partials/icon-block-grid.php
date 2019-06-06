926239b5701407583b526ca2099f84d4d7af67bf
Dana Malkin <dana@danamalkin.com>
2019-06-06 13:54:18 -0700

    added alt tags to all images except in two col block

--- parts/global/partials/icon-block-grid.php
+++ parts/global/partials/icon-block-grid.php
@@ -11,7 +11,9 @@ if( have_rows('icon_blocks') ) :
 
 
       <?php while( have_rows('icon_blocks') ) : the_row();
-        $img = wp_get_attachment_image_src(get_sub_field('icon'), 'column');
+        $img_id = get_sub_field('icon');
+        $img = wp_get_attachment_image_src($img_id, 'column');
+        $alt_text = get_post_meta($img_id , '_wp_attachment_image_alt', true);
         $text = get_sub_field('text');
 
         // Conditional animation
@@ -20,7 +22,7 @@ if( have_rows('icon_blocks') ) :
         <div class="row" data-aos="<?php echo $animation; ?>" data-aos-duration="800" data-aos-delay="500">
           <div class="col-5 sm-col-10 col-centered container icon-block-grid__column">
             <div class="icon">
-              <img src="<?php echo $img[0]; ?>" alt="Response stage icon">
+              <img src="<?php echo $img[0]; ?>" alt="<?php echo $alt_text; ?>">
             </div>
 
             <div class="text">
--- single.php
+++ single.php
@@ -9,13 +9,21 @@
   // variables
   $thumbnail = get_the_post_thumbnail();
 
-  if( have_posts() ): while( have_posts() ): the_post(); ?>
+  if( have_posts() ): while( have_posts() ): the_post();
+
+    //variables
+    if ( $alt_text = get_the_post_thumbnail_caption() ) {
+        // Nothing to do here
+    } else {
+        $alt_text = get_the_title();
+    }
+  ?>
 
     <section class="container blog blog--single">
 
       <div class="row">
         <div class="col-10 sm-col-11 col-centered">
-          <img class="blog--single__featured-image" src="<?php the_post_thumbnail_url( 'full' ); ?>" />
+          <img class="blog--single__featured-image" src="<?php the_post_thumbnail_url( 'full' ); ?>" alt="<?php echo $alt_text; ?>" />
           <h2><?php the_title(); ?></h2>
           <h6><?php the_date(); ?></h6>
           <?php the_content(); ?>
